3ba5009d13747c7b4ac81453c114f58dfe7cafc4
Kanwarjit Rupyal <kanwar.rupyal@gmail.com>
2018-12-11 15:47:55 -0600

    added more functionality, added more styling to console. deleted another un-necessary function.

--- myScript.js
+++ myScript.js
@@ -7,26 +7,12 @@
 //5) If People bets the maximum(max die roll) and if player wins that bet, he wins the pot(MONEY) with the multiplier(die roll).
 
 
-
+// One Single DIE ROLL function
 function dieRoll(numSides){
 	let dieRollNumber = Math.floor(Math.random()*numSides)+1;
 	return dieRollNumber;
 }
 
-// function placeBet(amount){
-// 	let betPlaced = dieRoll(amount);
-// 	return betPlaced;
-// }
-
-// function drinkBeer(numBottles){
-// 	let beer2Drink = dieRoll(numBottles);
-// 	return beer2Drink;
-// }
-
-// function beerDrunk(numBottles){
-// 	let bottlesDrunk = dieRoll(numBottles);
-// 	return bottlesDrunk;
-// }
 
 function betMultiplier(){
 	let multiplier = dieRoll(4);
@@ -63,7 +49,7 @@ function lastManStanding(){
 	let round = 0;
 	while(player1Health > 0 && player1Money > 0 && player1Energy > 0 && player2Health > 0 && player2Money > 0 && player2Energy > 0){
 		round++;
-		console.log("%cRound"+round, "background: rgba(0,0,200,0.3); color: yellow");
+		console.log("%cRound"+round, "background: rgba(0,0,200,0.3); color: red; font-family: impact; font-size: 25px");
 		let playerBet = dieRoll(10)*5;
 		let peopleBet = dieRoll(20)*5;
 		let totalBet = playerBet*2 + peopleBet;
@@ -72,18 +58,18 @@ function lastManStanding(){
 		let player1BottlesDrunk = dieRoll(6);
 		let player2BottlesDrunk = dieRoll(6);
 		
-		if(round % 2 == 1){
+		if(round % 2 == 1){		//for Odd Rounds, When player 1 challenges player 2
 			console.log("Player 1 challenges Player 2 to drink "+player2DrinkDare+" beer bottles.");
 			console.log("Bet placed by each player : $"+playerBet);
 			console.log("Bets placed by people: $"+peopleBet);
 			console.log("Total Pot is now: $"+totalBet);
-			if(checkMax(20, peopleBet))
+			if(checkMax(20, peopleBet))					// checks for if people placed the maximum bet
 			{
 				let multiplier = dieRoll(4);
-				console.log("People have bet the MAXIMUM!, winning money is "+multiplier+" times now!");
-				totalBet = totalBet*multiplier;
+				console.log("%cPeople have bet the MAXIMUM!, winning money is "+multiplier+" times now!", "background: rgba(0,0,200,0.3); color: red");
+				totalBet = totalBet*multiplier;			// if people placed the max bet, total pot will get increased as per the multiplier
 			}
-			if(player2BottlesDrunk >= player2DrinkDare)
+			if(player2BottlesDrunk >= player2DrinkDare) //if player 2 successfully wins the challenge
 				{
 			
 					player2Money += totalBet;
@@ -93,12 +79,13 @@ function lastManStanding(){
 					player1Energy -= changeEnergy(8);
 					console.log("Player 2 successfully drank "+player2DrinkDare+" bottles");
 					console.log("Player 2 Wins the bet");
-					console.log("Player 2 Current Money: $"+player2Money+" Player 1 Current Money: $"+player1Money);	
+					console.log("Player 2 Current Money: $"+player2Money);
+					console.log("Player 1 Current Money: $"+player1Money);	
 					console.log("Player 2 lost some health, current health is: "+player2Health);
 				}
-				else
+			else
 				{
-					player1Money += totalBet;
+					player1Money += totalBet;	//if player 2 unable to win challenge
 					player2Money -= playerBet;
 					player2Health -= changeHealth(12);
 					console.log("Player 2 was unable to drink "+player2DrinkDare+" bottles");
@@ -108,41 +95,41 @@ function lastManStanding(){
 					console.log("Player 2 lost some health, Current Health is: "+player2Health);
 				}
 		}
-		else if(round % 2 == 0){
+		else if(round % 2 == 0){					//for even rounds, when player 2 challenges player 1
 			console.log("Player 2 challenges Player 1 to drink "+player1DrinkDare+" beer bottles.");
 			console.log("Bet placed by each player: $"+playerBet);
 			console.log("Bets placed by people: $"+peopleBet);
 			console.log("Total Pot is now: $"+totalBet);
 			if(checkMax(20, peopleBet))
-			{
-				let multiplier = dieRoll(4);
-				console.log("People have bet the MAXIMUM!, winning money is "+multiplier+" times now!");
-				totalBet = totalBet*multiplier;
-			}
+				{
+					let multiplier = dieRoll(4);
+					console.log("%cPeople have bet the MAXIMUM!, winning money is "+multiplier+" times now!", "background: rgba(0,0,200,0.3); color: red");
+					totalBet = totalBet*multiplier;
+				}
 
-			if(player1BottlesDrunk >= player1DrinkDare)
-			{
-			
-				player1Money += totalBet;
-				player2Money -= playerBet; 
-				player1Health -= changeHealth(12);
-				console.log("Player 1 successfully drank "+player1DrinkDare+" bottles");
-				console.log("Player 1 Wins the bet");
-				console.log("Player 1 Money is now: $"+player1Money);
-				console.log("Player 2 Money is now: $"+player2Money);
-				console.log("Player 1 lost some health, current health is: "+player1Health);	
-			}
-			else
-			{
-				player2Money += totalBet;
-				player1Money -= playerBet;
-				player1Health -= changeHealth(12);
-				console.log("Player 1 was unable to drink "+player1DrinkDare+" bottles");
-				console.log("Player 2 wins the bet");
-				console.log("Player 2 Money is now: $"+player2Money);
-				console.log("Player 1 Money is now: $"+player1Money);
-				console.log("Player 1 lost some health, Current Health is: "+player1Health);
-			}
+			if(player1BottlesDrunk >= player1DrinkDare)			//if player 1 wins the challenge
+				{
+		
+					player1Money += totalBet;
+					player2Money -= playerBet; 
+					player1Health -= changeHealth(12);
+					console.log("Player 1 successfully drank "+player1DrinkDare+" bottles");
+					console.log("Player 1 Wins the bet");
+					console.log("Player 1 Money is now: $"+player1Money);
+					console.log("Player 2 Money is now: $"+player2Money);
+					console.log("Player 1 lost some health, current health is: "+player1Health);	
+				}
+			else								// if player 1 does not win the challenge
+				{
+					player2Money += totalBet;
+					player1Money -= playerBet;
+					player1Health -= changeHealth(12);
+					console.log("Player 1 was unable to drink "+player1DrinkDare+" bottles");
+					console.log("Player 2 wins the bet");
+					console.log("Player 2 Money is now: $"+player2Money);
+					console.log("Player 1 Money is now: $"+player1Money);
+					console.log("Player 1 lost some health, Current Health is: "+player1Health);
+				}
 		}
 	}
 	checkWinner(player1Health, player2Health, player1Money, player2Money, player1Energy, player2Energy);
@@ -156,16 +143,15 @@ function checkWinner(player1Health, player2Health, player1Money, player2Money, p
 	if(player1Health <= 0 || player1Money <= 0 || player1Energy <= 0)
 	{
 		console.log("Player 1 lost the game");
-		console.log("PLAYER 2 WINS !");
+		console.log("%cPLAYER 2 WINS !", "font-family: impact; font-size:35px;letter-spacing: 1px");
+		document.getElementById("displayWin").innerHTML = "PLAYER 2 WINS. GAME OVER"
 	}
 	else if(player2Health <= 0 || player2Money <= 0 || player2Energy <= 0)
 	{
 		console.log("Player 2 lost the game");
-		console.log("PLAYER 1 WINS !");
+		console.log("%cPLAYER 1 WINS !","font-family: impact;font-size: 35px;letter-spacing: 1px");
+		document.getElementById("displayWin").innerHTML = "PLAYER 1 WINS. GAME OVER"
 	}
 }
 
-function checkMoney(player1Money, player2Money){
 
-
-}
